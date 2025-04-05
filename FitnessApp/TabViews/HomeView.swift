40cdce471ce261c6da631d043d73292a65153a53
Ved Prakash Mishra <vedmi74@gmail.com>
2025-04-06 00:12:56 +0530

    Tabbar

---
+++ FitnessApp/TabViews/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  FitnessApp
+//
+//  Created by Ved Prakash Mishra on 05/04/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ FitnessApp/Tabbar/FitnessTabView.swift
@@ -0,0 +1,42 @@
+//
+//  FitnessTabView.swift
+//  FitnessApp
+//
+//  Created by Ved Prakash Mishra on 05/04/25.
+//
+
+import SwiftUI
+
+struct FitnessTabView: View {
+    @State var selectedTab = "Home"
+    
+    init(){
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.stackedLayoutAppearance.selected.iconColor = .green
+        
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+    }
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            HomeView()
+                .tag( "Home" )
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            
+            HistoricDataView()
+                .tag("historic")
+                .tabItem {
+                    Image(systemName: "chart.line.uptrend.xyaxis")
+                    Text("Charts")
+                }
+        }
+
+    }
+}
+
+#Preview {
+    FitnessTabView()
+}
